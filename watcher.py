3dea77c3ec8c9bbb90767dc07547bc7c852901c6
9mht9y2yjv-beep <9mht9y2yjv@privaterelay.appleid.com>
2026-08-29 19:06:07 +0300

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -1,4 +1,5 @@
-usr/bin/env python3
+#!/usr/bin/env python3
+
 """
 Noon Minutes Discount Watcher — v4.0 (Smart Memory & Full Catalog Sweep)
 ------------------------------------------------------------------
